2d6b1aaba57b8b314349f29b8f22868f795d7f0e
INQUIRER-LAB <tayez788@gmail.com>
2019-11-16 15:22:35 +0900

    add IntManager
    sumメソッド作成

--- GitEnjoy/Program.cs
+++ GitEnjoy/Program.cs
@@ -9,4 +9,13 @@ namespace GitEnjoy
             Console.WriteLine("Hello World!");
         }
     }
+    public class IntManager{
+        public static int Sum(int[] array){
+            int j;
+            for(int i = 0; i < array.Length; i++){
+                j += array[i];
+            }
+            return j;
+        }
+    }
 }
